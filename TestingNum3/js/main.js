16720d7811d9463cc74ca9b37468e3365ed40546
flemons <47301212+flemons@users.noreply.github.com>
2021-02-01 01:16:05 -0500

    Nav Addition

--- TestingNum3/js/main.js
+++ TestingNum3/js/main.js
@@ -29,3 +29,19 @@
   document.addEventListener('DOMContentLoaded', loadbar, false);
 }());
 
+function isElementInViewport (el) {
+
+    // Special bonus for those using jQuery
+    if (typeof jQuery === "function" && el instanceof jQuery) {
+        el = el[0];
+    }
+
+    var rect = el.getBoundingClientRect();
+
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /* or $(window).height() */
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth) /* or $(window).width() */
+    );
+}
